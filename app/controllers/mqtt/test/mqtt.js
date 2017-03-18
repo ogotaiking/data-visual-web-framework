51adbb15bcc74aae3fc8cc961e32aaabd04a8cd3
ogotaiking <ogotaiking@gmail.com>
2017-03-18 23:50:23 +0800

    mqtt listen

--- app/controllers/mqtt/test/mqtt.js
+++ app/controllers/mqtt/test/mqtt.js
@@ -2,6 +2,13 @@ var mqtt = require('mqtt')
 var client = mqtt.connect('mqtt://127.0.0.1')
 
 client.on('connect',function () {
-    client.subscribe('topic')
-    client.publish('topic','hello')
+    client.subscribe('#')
+
+    client.publish('topicA','hello')
 })
+
+    client.on('message', function (topic, message) {  
+        // message is Buffer  
+        console.log(topic.toString(),message.toString());  
+        //client.end();  
+    });  
